6120fab9c2b3c55187102bfb2a0fa58d62ed22f2
Michael Levy <milevy@redhat.com>
2021-11-11 00:34:36 +0200

    build image no cache (#13)

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,6 +5,7 @@ set -exu
 TAG=$(git rev-parse --short=7 HEAD)
 REPO="quay.io/app-sre/assisted-events-scrape"
 export ASSISTED_EVENTS_SCRAPE_IMAGE="${REPO}:${TAG}"
+export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
 make build-image
 docker login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
