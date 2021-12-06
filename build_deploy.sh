5e732df4501ab87d304b5f16b896169ac4cf130f
Michael Levy <milevy@redhat.com>
2021-12-06 14:53:01 +0200

    Building and pushing kibana image

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,7 +7,13 @@ REPO="quay.io/app-sre/assisted-events-scrape"
 export ASSISTED_EVENTS_SCRAPE_IMAGE="${REPO}:${TAG}"
 export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
 make build-image
+
+KIBANA_REPO="quay.io/app-sre/kibana"
+export KIBANA_IMAGE="${KIBANA_REPO}:7.10.2"
+make build-kibana-image
+
 docker login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
 docker push "${ASSISTED_EVENTS_SCRAPE_IMAGE}"
 docker push "${REPO}:latest"
+docker push "${KIBANA_IMAGE}"
