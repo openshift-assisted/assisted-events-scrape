ab79228533c2f26cc5d16bc8a26442a58dccae0f
Riccardo Piccoli <rpiccoli@redhat.com>
2022-03-29 19:23:47 +0200

    fix client version to avoid erros (#53) (#55)

--- build_deploy.sh
+++ build_deploy.sh
@@ -8,14 +8,9 @@ export ASSISTED_EVENTS_SCRAPE_IMAGE="${REPO}:${TAG}"
 export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
 make build-image
 
-KIBANA_REPO="quay.io/app-sre/kibana-oss"
-export KIBANA_IMAGE="${KIBANA_REPO}:7.10.2"
-make build-kibana-image
-
 DOCKER_CONF="$PWD/assisted/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
 docker --config="$DOCKER_CONF" push "${ASSISTED_EVENTS_SCRAPE_IMAGE}"
 docker --config="$DOCKER_CONF" push "${REPO}:latest"
-docker --config="$DOCKER_CONF" push "${KIBANA_IMAGE}"
