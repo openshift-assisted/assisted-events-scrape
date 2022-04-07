5077197f218a2ab4792c5d00b299d444cb3477be
Riccardo Piccoli <rpiccoli@redhat.com>
2022-04-07 10:25:34 +0200

    simplify transformation to k8s manifests (#73)

--- tools/ocp2k8s.sh
+++ tools/ocp2k8s.sh
@@ -2,22 +2,17 @@
 image=$1
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Get currently exported variables
-current_envs=$(env | grep -f <(cat ${SCRIPT_DIR}/../openshift/template.yaml | yq --raw-output '.parameters[] | "\(.name)"'))
-
-# Extract variables from parameters
-eval $(cat ${SCRIPT_DIR}/../openshift/template.yaml | yq --raw-output '.parameters[] | "export \(.name)=\"\(.value)\""')
-
-# Override parameters env var with current env vars
-exportable_vars=$(echo ${current_envs} | sed 's/^/export /g')
-if [[ ! -z "${current_envs// }" ]]; then
-    eval "${exportable_vars}"
-fi
+template_file=${SCRIPT_DIR}/../openshift/template.yaml
 
 export IMAGE_NAME=$(echo $image | cut -d: -f1)
 export IMAGE_TAG=$(echo $image | cut -d: -f2)
+export IMAGE_PULL_POLICY=Never
 
-# Substitute variables named in parameters
-envs=$(cat ${SCRIPT_DIR}/../openshift/template.yaml | yq --raw-output '.parameters[] | "${\(.name)}"' | tr '\n' ' ')
+# Get currently exported variables
+current_envs=$(env | grep -f <(oc process --local=true -f openshift/template.yaml --parameters=true | tail -n+2 | awk '{ print $1 }') | tr '\n' ' ')
 
-cat ${SCRIPT_DIR}/../openshift/template.yaml | sed 's/{{/{/g' | sed 's/}}/}/g' | envsubst "${envs}" | yq -y '.objects[]' | sed 's/imagePullPolicy: Always/imagePullPolicy: Never/g'
+param=""
+if [[ ! -z ${current_envs} ]]; then
+   param="--param ${current_envs}"
+fi
+oc process --local=true -f openshift/template.yaml --output=yaml $param
