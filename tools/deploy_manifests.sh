eae433ad1341690fe6191298ccf087cb4e1e0a28
Riccardo Piccoli <rpiccoli@redhat.com>
2022-04-10 10:09:13 +0200

    fix CI integration test (#75)

--- tools/deploy_manifests.sh
+++ tools/deploy_manifests.sh
@@ -18,7 +18,7 @@ ${cli} get ns ${namespace} || ${cli} create ns ${namespace}
 ${cli} apply ${ns} -f assisted-events-scrape/tests/integration/manifests
 
 if [[ "${platform}" == "ocp" ]]; then
-    ${cli} process --local=true -f assisted-events-scrape/tests/integration/ci-manifests/ --output=yaml --param ELASTICSEARCH_ROUTE_HOST=$(./tools/get_ocp_route_host.sh) | ${cli} apply -f -
+    ${cli} process --local=true -f assisted-events-scrape/tests/integration/ci-manifests/ --output=yaml --param ELASTICSEARCH_ROUTE_HOST=$(./tools/get_ocp_route_host.sh) | ${cli} apply ${ns} -f -
 fi
 
 
@@ -27,7 +27,9 @@ ${cli} wait ${ns} --timeout=300s --for=condition=Ready pods --all
 
 # Deploy assisted-events-scraper
 if [[ "${platform}" == "ocp" ]]; then
-    ${cli} apply ${ns} -f openshift/
+    image_name=$(echo $ASSISTED_EVENTS_SCRAPE_IMAGE | cut -d: -f1)
+    image_tag=$(echo $ASSISTED_EVENTS_SCRAPE_IMAGE | cut -d: -f2)
+    ${cli} process --output=yaml ${ns} -f openshift/template.yaml --param IMAGE_NAME=${image_name} IMAGE_TAG=${image_tag} | ${cli} apply ${ns} -f -
 else
     ./tools/ocp2k8s.sh ${image} | ${cli} apply ${ns} -f -
 fi
