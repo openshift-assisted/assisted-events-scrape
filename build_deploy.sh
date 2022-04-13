dec970268d11e3c72870165fc98eef16168444a7
Riccardo Piccoli <rpiccoli@redhat.com>
2022-04-13 16:49:20 +0200

    produce valid image for app-sre (#80)

--- build_deploy.sh
+++ build_deploy.sh
@@ -8,9 +8,15 @@ export ASSISTED_EVENTS_SCRAPE_IMAGE="${REPO}:${TAG}"
 export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
 make build-image
 
+export ELASTICDUMP_IMAGE_REPO="quay.io/app-sre/elasticdump"
+export ELASTICDUMP_IMAGE_TAG="v6.82.3"
+export ELASTICDUMP_ORIGINAL_REPO="elasticdump/elasticsearch-dump"
+
 DOCKER_CONF="$PWD/assisted/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
 docker --config="$DOCKER_CONF" push "${ASSISTED_EVENTS_SCRAPE_IMAGE}"
 docker --config="$DOCKER_CONF" push "${REPO}:latest"
+docker tag "${ELASTICDUMP_ORIGINAL_REPO}:${ELASTICDUMP_IMAGE_TAG}" "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
