f7245b9a7723a5091f307535c25ef4bd79c142a5
Riccardo Piccoli <rpiccoli@redhat.com>
2022-04-13 17:39:22 +0200

    pull elasticdump image before tagging (#81)

--- build_deploy.sh
+++ build_deploy.sh
@@ -18,5 +18,6 @@ docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.i
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
 docker --config="$DOCKER_CONF" push "${ASSISTED_EVENTS_SCRAPE_IMAGE}"
 docker --config="$DOCKER_CONF" push "${REPO}:latest"
+docker pull "${ELASTICDUMP_ORIGINAL_REPO}:${ELASTICDUMP_IMAGE_TAG}"
 docker tag "${ELASTICDUMP_ORIGINAL_REPO}:${ELASTICDUMP_IMAGE_TAG}" "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
 docker --config="$DOCKER_CONF" push "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
