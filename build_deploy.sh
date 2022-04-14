3dc4848ae8d3f01d42c777ef74a779d6ee89e10c
Riccardo piccoli <rpiccoli@redhat.com>
2022-04-14 09:59:38 +0200

    rebuild elasticdump image

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,11 +6,10 @@ TAG=$(git rev-parse --short=7 HEAD)
 REPO="quay.io/app-sre/assisted-events-scrape"
 export ASSISTED_EVENTS_SCRAPE_IMAGE="${REPO}:${TAG}"
 export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
-make build-image
+export ELASTICDUMP_IMAGE="quay.io/app-sre/elasticdump:v6.82.3"
 
-export ELASTICDUMP_IMAGE_REPO="quay.io/app-sre/elasticdump"
-export ELASTICDUMP_IMAGE_TAG="v6.82.3"
-export ELASTICDUMP_ORIGINAL_REPO="elasticdump/elasticsearch-dump"
+make build-image
+make build-elasticdump-image
 
 DOCKER_CONF="$PWD/assisted/.docker"
 mkdir -p "$DOCKER_CONF"
@@ -18,6 +17,4 @@ docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.i
 docker tag "${ASSISTED_EVENTS_SCRAPE_IMAGE}" "${REPO}:latest"
 docker --config="$DOCKER_CONF" push "${ASSISTED_EVENTS_SCRAPE_IMAGE}"
 docker --config="$DOCKER_CONF" push "${REPO}:latest"
-docker pull "${ELASTICDUMP_ORIGINAL_REPO}:${ELASTICDUMP_IMAGE_TAG}"
-docker tag "${ELASTICDUMP_ORIGINAL_REPO}:${ELASTICDUMP_IMAGE_TAG}" "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
-docker --config="$DOCKER_CONF" push "${ELASTICDUMP_IMAGE_REPO}:${ELASTICDUMP_IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${ELASTICDUMP_IMAGE}"
