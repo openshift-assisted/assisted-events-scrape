4adb407aa6720de67fd015012463d1b5e3cd595a
Riccardo piccoli <rpiccoli@redhat.com>
2022-05-25 16:40:05 +0200

    add default protocol

--- assisted-events-scrape/config/object_storage.py
+++ assisted-events-scrape/config/object_storage.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from utils import get_env
 
 
+DEFAULT_S3_ENDPOINT_PROTOCOL = "s3://"
+
+
 @dataclass
 class ObjectStorageConfig:
     access_key: str
@@ -11,6 +14,9 @@ class ObjectStorageConfig:
 
     @classmethod
     def create_from_env(cls) -> 'ObjectStorageConfig':
+        endpoint = get_env("AWS_S3_BUCKET")
+        if "://" not in endpoint:
+            endpoint = DEFAULT_S3_ENDPOINT_PROTOCOL + endpoint
         return cls(
             get_env("AWS_ACCESS_KEY_ID"),
             get_env("AWS_SECRET_ACCESS_KEY"),
