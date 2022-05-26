2f35fc25249df282552eb7b5e77261d258503072
Riccardo piccoli <rpiccoli@redhat.com>
2022-05-26 10:24:25 +0200

    change default protocol for s3 to https

--- assisted-events-scrape/config/object_storage.py
+++ assisted-events-scrape/config/object_storage.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from utils import get_env
+from utils import get_env, log
 
 
-DEFAULT_S3_ENDPOINT_PROTOCOL = "s3://"
+DEFAULT_S3_ENDPOINT_PROTOCOL = "https://"
 
 
 @dataclass
@@ -14,9 +14,10 @@ class ObjectStorageConfig:
 
     @classmethod
     def create_from_env(cls) -> 'ObjectStorageConfig':
-        endpoint = get_env("AWS_S3_BUCKET")
+        endpoint = get_env("AWS_S3_ENDPOINT")
         if "://" not in endpoint:
             endpoint = DEFAULT_S3_ENDPOINT_PROTOCOL + endpoint
+        log.info(f"Setting endpoint {endpoint}")
         return cls(
             get_env("AWS_ACCESS_KEY_ID"),
             get_env("AWS_SECRET_ACCESS_KEY"),
